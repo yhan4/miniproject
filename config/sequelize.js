88fca4cb7e1fef49bed9cc1889f80a2d87b672e4
yhan4 <yhan.4@hotmail.com>
2024-02-05 23:33:02 -0800

    Currency and Country endpoints using Sequelize

--- config/sequelize.js
+++ config/sequelize.js
@@ -1,12 +1,17 @@
 const { Sequelize } = require('sequelize')
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+const sequelize = new Sequelize(
+    process.env.DB_NAME, 
+    process.env.DB_USER, 
+    process.env.DB_PASSWORD, 
+    {
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     dialect: 'postgres',
     dialectOptions: {
       ssl: {
           require: true,
-          rejectionUnauthorized:true,
+          rejectUnauthorized:true,
       }
     }
   });
--- utils/middleware.js
+++ utils/middleware.js
@@ -1,73 +1,88 @@
-let currencies = [
-    {
-      id: 1,
-      currencyCode: "CDN",
-      country: "Canada",
-      conversionRate: 1
-    },
-    {
-      id: 2,
-      currencyCode: "USD",
-      country: "United States of America",
-      conversionRate: 0.75
-    }
-]
+// let currencies = [
+//     {
+//       id: 1,
+//       currencyCode: "CDN",
+//       country: "Canada",
+//       conversionRate: 1
+//     },
+//     {
+//       id: 2,
+//       currencyCode: "USD",
+//       country: "United States of America",
+//       conversionRate: 0.75
+//     }
+// ]
+const Currency = require('../models/currency')
 
-const getCurrencies = (request, response) => {
+const getCurrencies = async(request, response) => {
+  try {
+    const currencies = await Currency.findAll()
     response.json(currencies)
-}
-
-const getCurrencyById = (request, response) => {
-    // extract id from URL
-  const id = request.params.id
-  const curreny = currencies.find(c => c.id === parseInt(id))
-
-  if (!curreny) {
-    return response.status(404).json({error: 'resource not found' })
+  } catch (error) {
+    console.error(error)
   }
-
-  response.json(curreny)
 }
 
-const addCurrency = (request, response) => {
+const getCurrencyById = async(request, response) => {
+  // extract id from URL
+  const id = request.params.id
+  try {
+    const curreny = await Currency.findByPk(id)
+    if (!curreny) {
+     return response.status(404).json({error: 'resource not found' })
+    }
+    response.json(curreny)
+  } catch (error) {
+    console.error(error)
+  }
+  
+}
+
+const addCurrency = async (request, response) => {
     // extract data sent in the request body
   const currencyCode = request.body
   const country = request.body
   const conversionRate = request.body
 
-  if (!currencyCode||!country||!conversionRate ) {
-    return response.status(400).json({ error: 'content missing' })
+  try {
+    const newCurrency = await Currency.create({
+      currencyCode,
+      counryID,
+      conversionRate
+    })
+    if (!currencyCode||!country||!conversionRate ) {
+      return response.status(400).json({ error: 'content missing' })
+    }
+    response.json(newCurrency)
+  } catch {
+    console.error(error)
   }
-
-  const EURcurrency = {
-    id: currencies.length + 1,
-    currencyCode: "EUR",
-    country: "European Union",
-    conversionRate: 0.69
-  };
-
-  currencies.push(EURcurrency)
-
-  response.json(EURcurrency)
 }
 
-const updateCurrency = (request, response) => {
-    // extract data from request parameters
+const updateCurrency = async (request, response) => {
   const id = request.params.id
   const newRate = request.params.newRate
-  const currencyID = parseInt(id)
-  const currencyUpdate = currencies.find(c => c.id === currencyID) 
-
-  currencyUpdate.conversionRate = newRate
-  response.json(currencyUpdate)
+  
+  try {
+    const currency = await Currency.findByPk(id) 
+    currency.conversionRate = newRate
+    await currency.save()
+    response.json(currency)
+  } catch (error) {
+    console.error(error)
+  }
 }
 
-const deleteCurrency = (request, response) => {
-    const id = parseInt(request.params.id)
+const deleteCurrency = async (request, response) => {
+  const id = parseInt(request.params.id)
 
-  currencies = currencies.filter(currency => currency.id !== id)
-
-  response.status(204).json()
+  try {
+    const currency = await Currency.findByPk(id)
+    await currency.destroy()
+    response.status(204).json()
+  } catch (error) {
+    console.error(error)
+  }  
 }
 
 module.exports = {
